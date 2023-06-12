75c9bd526bdd2ce468d74107153ea0e7c35def2b
Jerome Swannack <jeromeswannack@gmail.com>
2023-06-12 16:39:27 +1200

    Added custom tool using BaseTool

--- homegpt/tools/shell.py
+++ homegpt/tools/shell.py
@@ -1,30 +1,51 @@
 # Import things that are needed generically
+from typing import Type, Optional
 from langchain import LLMMathChain
 from langchain.agents import AgentType, initialize_agent
 from langchain.chat_models import ChatOpenAI
 from langchain.tools import BaseTool, StructuredTool, Tool, tool
+from pydantic import BaseModel, Field
+from langchain.callbacks.manager import (
+    AsyncCallbackManagerForToolRun,
+    CallbackManagerForToolRun,
+)
 
 
-def tool_function(s: str) -> str:
-    return s[::-1]
+class ReverseInput(BaseModel):
+    """Input to the reverse tool."""
+
+    text: str = Field(..., description="The text to reverse.")
+
+
+class ReverseTool(BaseTool):
+    name = "reverse"
+    description = "Useful for reversing text."
+    args_schema: Type[BaseModel] = ReverseInput
+
+    def _run(
+        self, query: str, run_manager: Optional[CallbackManagerForToolRun] = None
+    ) -> str:
+        """Use the tool."""
+        return query[::-1]
+
+    async def _arun(
+        self, query: str, run_manager: Optional[AsyncCallbackManagerForToolRun] = None
+    ) -> str:
+        """Use the tool asynchronously."""
+        raise NotImplementedError("Reverse does not support async")
 
 
 def main():
     from homegpt.llm import get_llm
 
-    tools = [
-        Tool.from_function(
-            func=tool_function,
-            name="reverse",
-            description="Useful for when you want to reverse a string.",
-        ),
-    ]
+    tools = [ReverseTool()]
     llm = get_llm()
     agent = initialize_agent(
         tools=tools,
         llm=llm,
         agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
         verbose=True,
+        max_iterations=4,
     )
 
     agent.run("reverse this string: hello world")
