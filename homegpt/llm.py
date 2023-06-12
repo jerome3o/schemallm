f17f7c96215e9d924d6b8bf14a455b8be0bae6b0
Jerome Swannack <jeromeswannack@gmail.com>
2023-06-12 18:10:42 +1200

    Updated chat llm api

--- homegpt/llm.py
+++ homegpt/llm.py
@@ -1,6 +1,7 @@
 import os
 
-from langchain.llms import OpenAI, OpenAIChat
+from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
 
 _openai_api_base = os.environ["OPENAI_API_BASE"]
 _model = "vicuna-13b-v1.1-8bit"
@@ -15,8 +16,8 @@ def get_llm() -> OpenAI:
     )
 
 
-def get_llm_chat() -> OpenAIChat:
-    return OpenAIChat(
+def get_llm_chat() -> ChatOpenAI:
+    return ChatOpenAI(
         temperature=0,
         openai_api_base=_openai_api_base,
         openai_api_key="no",
