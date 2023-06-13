3e30e250674713b94655eab22ff87f8d2ebbd4fe
Jerome Swannack <jeromeswannack@gmail.com>
2023-06-13 22:41:31 +1200

    Finished custom agent

--- homegpt/agent.py
+++ homegpt/agent.py
@@ -64,20 +64,68 @@ class CustomPromptTemplate(StringPromptTemplate):
         return self.template.format(**kwargs)
 
 
+reverse_tool = Tool(
+    name="Reverse",
+    description="Reverses a string.",
+    func=lambda s: s[::-1],
+)
+tools = [reverse_tool]
+
+
+prompt = CustomPromptTemplate(
+    template=template,
+    tools=tools,
+    # This omits the `agent_scratchpad`, `tools`, and `tool_names` variables because those are generated dynamically
+    # This includes the `intermediate_steps` variable because that is needed
+    input_variables=["input", "intermediate_steps"],
+)
+
+
+class CustomOutputParser(AgentOutputParser):
+    def parse(self, llm_output: str) -> Union[AgentAction, AgentFinish]:
+        # Check if agent should finish
+        if "Final Answer:" in llm_output:
+            return AgentFinish(
+                # Return values is generally always a dictionary with a single `output` key
+                # It is not recommended to try anything else at the moment :)
+                return_values={"output": llm_output.split("Final Answer:")[-1].strip()},
+                log=llm_output,
+            )
+        # Parse out the action and action input
+        regex = r"Action\s*\d*\s*:(.*?)\nAction\s*\d*\s*Input\s*\d*\s*:[\s]*(.*)"
+        match = re.search(regex, llm_output, re.DOTALL)
+        if not match:
+            raise ValueError(f"Could not parse LLM output: `{llm_output}`")
+        action = match.group(1).strip()
+        action_input = match.group(2)
+        # Return the action and action input
+        return AgentAction(
+            tool=action, tool_input=action_input.strip(" ").strip('"'), log=llm_output
+        )
+
+
+output_parser = CustomOutputParser()
+
+
 def main():
     from homegpt.llm import get_llm_chat
 
-    reverse_tool = Tool(
-        name="Reverse",
-        description="Reverses a string.",
-        func=lambda s: s[::-1],
-    )
-    tools = [reverse_tool]
+    llm = get_llm_chat()
 
-    agent = FakeAgent()
+    # LLM chain consisting of the LLM and a prompt
+    llm_chain = LLMChain(llm=llm, prompt=prompt)
+
+    tool_names = [tool.name for tool in tools]
+    agent = LLMSingleActionAgent(
+        llm_chain=llm_chain,
+        output_parser=output_parser,
+        stop=["\nObservation:"],
+        allowed_tools=tool_names,
+    )
+
     agent_executor = AgentExecutor.from_agent_and_tools(
         agent=agent,
-        tools=[reverse_tool],
+        tools=tools,
         verbose=True,
     )
 
