583e1b93baaa9755591184b7bf26a96624c1d9c7
Jerome Swannack <jeromeswannack@gmail.com>
2023-06-14 22:29:01 +1200

    Updated chain example to use multiple inputs/outputs

--- tutorials/sequential_chain.py
+++ tutorials/sequential_chain.py
@@ -1,38 +1,57 @@
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
-from langchain.chains import SimpleSequentialChain
+from langchain.chains import SequentialChain
 
 from homegpt.llm import get_llm
 
 llm = get_llm()
 
 
-# This is an LLMChain to write a synopsis given a title of a play.
-template = """You are a playwright. Given the title of play, it is your job to write a \
-synopsis for that title.
+# This is an LLMChain to write a synopsis given a title of a play and the era it is set
+# in.
+template = """You are a playwright. Given the title of play and the era it is set in, \
+it is your job to write a synopsis for that title.
 
 Title: {title}
+Era: {era}
 Playwright: This is a synopsis for the above play:"""
-
-prompt_template = PromptTemplate(input_variables=["title"], template=template)
-synopsis_chain = LLMChain(llm=llm, prompt=prompt_template)
-
+prompt_template = PromptTemplate(input_variables=["title", "era"], template=template)
+synopsis_chain = LLMChain(
+    llm=llm,
+    prompt=prompt_template,
+    output_key="synopsis",
+)
 
 # This is an LLMChain to write a review of a play given a synopsis.
 template = """You are a play critic from the New York Times. Given the synopsis of play\
 , it is your job to write a review for that play.
 
-Play Synopsis:
+Title: {title}
+Synopsis:
 {synopsis}
 Review from a New York Times play critic of the above play:"""
-prompt_template = PromptTemplate(input_variables=["synopsis"], template=template)
-review_chain = LLMChain(llm=llm, prompt=prompt_template)
+prompt_template = PromptTemplate(
+    input_variables=["synopsis", "title"], template=template
+)
+review_chain = LLMChain(llm=llm, prompt=prompt_template, output_key="review")
 
 # This is the overall chain where we run these two chains in sequence.
 
-overall_chain = SimpleSequentialChain(
+overall_chain = SequentialChain(
     chains=[synopsis_chain, review_chain],
+    input_variables=["era", "title"],
+    # Here we return multiple variables
+    output_variables=["synopsis", "review"],
     verbose=True,
 )
 
-review = overall_chain.run("Tragedy at sunset on the beach")
+output = overall_chain(
+    {
+        "title": "Tragedy at sunset on the beach",
+        "era": "Victorian England",
+        "stop": "e",
+    }
+)
+
+print(output)
+print(output)
