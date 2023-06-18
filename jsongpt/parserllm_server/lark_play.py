c71e93e8c9d9b0d0a826fae4e77972f6352eb856
Jerome Swannack <jeromeswannack@gmail.com>
2023-06-18 04:04:07 +0000

    Made the lark example a bit more complicated

--- jsongpt/parserllm_server/lark_play.py
+++ jsongpt/parserllm_server/lark_play.py
@@ -1,17 +1,31 @@
-from lark import Lark, Transformer
+from lark import Lark, Transformer, v_args
+from lark.tree import Tree
 
 # Lark grammar based on EBNF
 json_grammar = r"""
     ?start: json_object
-    ?json_object: "{" season "," temperature "}"
+    ?json_object: "{" season "," temperature "," observations "}"
     ?season: season_key ":" season_value
     ?temperature: temperature_key ":" temperature_value
+    ?observations: observations_key ":" "[" [observation ("," observation)*] "]"
+
+    ?observation: "{" reporter "," value "}"
+    ?reporter: reporter_key ":" reporter_value
+    ?value: value_key ":" value_value
 
     temperature_key: "\"temperature_celsius\""
     temperature_value: SIGNED_NUMBER
 
     season_key: "\"season\""
     season_value: ESCAPED_STRING
+
+    observations_key: "\"observations\""
+
+    reporter_key: "\"reporter\""
+    reporter_value: ESCAPED_STRING
+
+    value_key: "\"value\""
+    value_value: SIGNED_NUMBER
 
     %import common.ESCAPED_STRING
     %import common.SIGNED_NUMBER
@@ -23,7 +37,7 @@ json_grammar = r"""
 parser = Lark(json_grammar, start="start", parser="lalr")
 
 # Test input
-test_json = '{"season": "winter", "bad_field": 5, "temperature_celsius": -5}'
+test_json = '{"season": "winter", "temperature_celsius": -5, "observations": [{"reporter": "John", "value": 1.2}, {"reporter": "Jane", "value": 3.4}]}'
 
-results = parser.parse(test_json)
-print(results)
+parse_tree = parser.parse(test_json)
+print(parse_tree)
