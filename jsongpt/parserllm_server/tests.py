0a5851c88c75667120cd6bf9dc07650e8e7b1ddb
Jerome Swannack <jeromeswannack@gmail.com>
2023-06-18 07:21:14 +0000

    Added test for unions

--- jsongpt/parserllm_server/tests.py
+++ jsongpt/parserllm_server/tests.py
@@ -54,9 +54,36 @@ def test_optional():
         print(parse_tree)
 
 
+def test_union():
+    class ReporterDetails(BaseModel):
+        name: str
+        age: int
+
+    class Observation(BaseModel):
+        reporter: Union[str, ReporterDetails]
+        value: float
+
+    schema = parse_json_schema(Observation.schema())
+    cfg = create_lark_cfg_for_schema(schema)
+
+    print(cfg)
+
+    parser = Lark(cfg, parser="lalr")
+
+    instances = [
+        Observation(reporter="hey", value=3.4).json(),
+        Observation(reporter={"name": "hey", "age": 3}, value=3.4).json(),
+    ]
+
+    for instance in instances:
+        parse_tree = parser.parse(instance)
+        print(parse_tree)
+
+
 def main():
     # test_mvp_schema()
-    test_optional()
+    # test_optional()
+    test_union()
 
 
 if __name__ == "__main__":
