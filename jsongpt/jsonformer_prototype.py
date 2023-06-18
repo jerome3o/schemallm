a8af12889028cc6224e00b7239c91105ef7358f4
Jerome Swannack <jeromeswannack@gmail.com>
2023-06-18 00:30:54 +0000

    Formatted jsonformer prototype

--- jsongpt/jsonformer_prototype.py
+++ jsongpt/jsonformer_prototype.py
@@ -16,7 +16,9 @@ class PersonDetails(BaseModel):
 
 def main():
     tokenizer = AutoTokenizer.from_pretrained(_model)
-    model = AutoModelForCausalLM.from_pretrained(_model, load_in_8bit=True, device_map="auto")
+    model = AutoModelForCausalLM.from_pretrained(
+        _model, load_in_8bit=True, device_map="auto"
+    )
 
     schema = PersonDetails.schema()
 
@@ -30,8 +32,8 @@ def main():
     print(result)
 
 
-
 if __name__ == "__main__":
     import logging
+
     logging.basicConfig(level=logging.INFO)
     main()
