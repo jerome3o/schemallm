290ca5d298ac1d5fc549894b2d30878df30e33d1
Jerome Swannack <jeromeswannack@gmail.com>
2023-06-26 09:30:59 +0000

    Removed codegen client and script - rolling my own

--- jsongpt-client/jsongpt_client/__init__.py
+++
@@ -1,7 +0,0 @@
-""" A client library for accessing Jsongpt """
-from .client import AuthenticatedClient, Client
-
-__all__ = (
-    "AuthenticatedClient",
-    "Client",
-)
--- jsongpt/scripts/generate_client.sh
+++
@@ -1,8 +0,0 @@
-# generates the client from the openapi spec
-
-## Install deps for this script:
-# pip install pipx
-# pipx install openapi-python-client --include-deps
-
-# run the server and get the spec
-openapi-python-generate --url http://localhost:8000/openapi.json
