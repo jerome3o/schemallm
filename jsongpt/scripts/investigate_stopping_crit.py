bc0a1dd4d894bb9cd4ed0c1d98066e7d0906d67d
Jerome Swannack <jeromeswannack@gmail.com>
2023-06-29 10:03:53 +0000

    Added more to the stopping criteria investigation

--- jsongpt/scripts/investigate_stopping_crit.py
+++ jsongpt/scripts/investigate_stopping_crit.py
@@ -10,18 +10,7 @@ from jsongpt.server.jsonschema2cfg import create_lark_cfg_for_schema
 _model = os.environ["MODEL_PATH"]
 
 
-def main():
-    pass
-
-    print("loading tokenizer")
-    tokenizer = AutoTokenizer.from_pretrained(_model, use_fast=False)
-    print("loading model")
-    model = AutoModelForCausalLM.from_pretrained(
-        _model,
-        load_in_8bit=True,
-        device_map="auto",
-    )
-
+def test_suite_1(model: AutoModelForCausalLM, tokenizer: AutoTokenizer):
     prompt = "Repeat after me: 14.000 is the best number.\nResonse:"
     response = complete_re(
         prompt,
@@ -57,8 +46,41 @@ def main():
     print(response)
 
 
+def main(model: AutoModelForCausalLM, tokenizer: AutoTokenizer):
+    class Details(BaseModel):
+        season: str
+        temperature_celsius: float
+
+    prompt = "Oh boy, it's cold outside! it must be winter and about 13.51 degrees celsius.\nJSON Object describing environment:\n"
+
+    cfg = create_lark_cfg_for_schema(Details.schema())
+    parser = Lark(cfg, start="start", parser="lalr")
+    response = complete_cf(
+        prompt=prompt,
+        parser=parser,
+        partial_completion="",
+        tokenizer=tokenizer,
+        model=model,
+        max_new_tokens=30,
+        debug=True,
+    )
+
+    print(response)
+    print(response)
+
+
 if __name__ == "__main__":
     import logging
 
     logging.basicConfig(level=logging.INFO)
-    main()
+
+    print("loading tokenizer")
+    tokenizer = AutoTokenizer.from_pretrained(_model, use_fast=False)
+    print("loading model")
+    model = AutoModelForCausalLM.from_pretrained(
+        _model,
+        load_in_8bit=True,
+        device_map="auto",
+    )
+
+    main(model, tokenizer)
