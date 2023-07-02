831aff647e53b32ce79bdea162d3377b6e882071
Jerome Swannack <jeromeswannack@gmail.com>
2023-07-02 20:14:08 +0000

    Added test script for the standard end point

---
+++ jsonllm/tests/test_completion_standard.py
@@ -0,0 +1,23 @@
+from jsonllm.models.api import CompletionRequest
+from jsonllm.server.server import complete_standard
+from jsonllm.server.load_model import load_model
+
+
+def main():
+    model, tokenizer = load_model()
+    complete_standard(
+        model=model,
+        tokenizer=tokenizer,
+        completion_request=CompletionRequest(
+            prompt="The name of three types of frogs:\n",
+            max_tokens=30,
+            temperature=0.7,
+        ),
+    )
+
+
+if __name__ == "__main__":
+    import logging
+
+    logging.basicConfig(level=logging.INFO)
+    main()
