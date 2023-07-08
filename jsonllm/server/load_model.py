280328579fb3c793ff1c32760fb01510dfeba91b
Jerome Swannack <jeromeswannack@gmail.com>
2023-07-08 00:31:42 +0000

    Removed device map for now, as it was causing issues https://github.com/huggingface/transformers/issues/22018

--- jsonllm/server/load_model.py
+++ jsonllm/server/load_model.py
@@ -3,12 +3,13 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 
 _model = os.environ["MODEL_PATH"]
 
+
 # TODO(j.swannack): These seem pointless, should remove or improve later
 def load_tokenizer(model: str = None):
     model = model or _model
     print("loading tokenizer")
     return AutoTokenizer.from_pretrained(
-        model, 
+        model,
         use_fast=False,
     )
 
@@ -19,5 +20,4 @@ def load_model(model: str = None):
     return AutoModelForCausalLM.from_pretrained(
         model,
         load_in_8bit=True,
-        device_map="auto",
     )
