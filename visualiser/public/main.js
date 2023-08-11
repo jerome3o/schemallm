519a48f9623dc5688e18c771ec5c7e99c2ae32c1
Jerome Swannack <jeromeswannack@gmail.com>
2023-08-11 06:32:15 +0000

    Added data/ to ignore, added basic data loading to main.js

--- visualiser/public/main.js
+++ visualiser/public/main.js
@@ -1,1 +1,9 @@
 console.log('Hello World');
+
+
+let data = {};
+
+$.getJSON("/data/infographic_data.json", function(response) {
+    data = response;
+    $("#stepSlider").attr("max", data.steps.length - 1);
+});
