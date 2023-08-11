ee89acbac132281eb78195505e668ce8d02229d4
Jerome Swannack <jeromeswannack@gmail.com>
2023-08-11 06:36:45 +0000

    Added text preview

--- visualiser/public/main.js
+++ visualiser/public/main.js
@@ -7,3 +7,23 @@ $.getJSON("/data/infographic_data.json", function(response) {
     data = response;
     $("#stepSlider").attr("max", data.steps.length - 1);
 });
+
+
+$("#stepSlider").on("input", function(e) {
+    updateVisualization(parseInt(e.target.value));
+});
+
+function updateVisualization(step) {
+    console.log(step);
+}
+
+function updateVisualization(stepIndex) {
+    const stepData = data.steps[stepIndex];
+
+    // Update the text preview
+    $("#partialCompletionText").text(stepData.partial_completion);
+    $("#selectedTokenText").text(stepData.selected_token);
+
+    // Update the bar plot, todo
+    // updateBarPlot(stepData);
+}
